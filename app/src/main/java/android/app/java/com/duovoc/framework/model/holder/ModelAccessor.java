8e093361376c3ebe2dd737d9f3386c5d48219bb7
Kato Shinya <yourdream28@gmail.com>
2019-10-13 19:29:17 +0900

    fixed

--- app/src/main/java/android/app/java/com/duovoc/framework/model/holder/ModelAccessor.java
+++ app/src/main/java/android/app/java/com/duovoc/framework/model/holder/ModelAccessor.java
@@ -1,4 +1,12 @@
 package android.app.java.com.duovoc.framework.model.holder;
 
 public abstract class ModelAccessor {
+  
+  private static final String TAG = ModelAccessor.class.getSimpleName();
+  
+  private final EnumMap<Class, Object> currentModelMap;
+  
+  protected ModelAccessor(Class mapKeys) {
+      this.currentModelMap = new EnumMap<>(mapKeys);
+  }
 }
