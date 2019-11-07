ee524b4bf944a3fa3579a45e81db81ca93078a64
myConsciousness <yourdream28@gmail.com>
2019-11-07 23:55:17 +0900

    Fixed

--- app/src/test/java/android/app/ks/thinkit/duovoc/ExampleUnitTest.java
+++ app/src/test/java/android/app/ks/thinkit/duovoc/ExampleUnitTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 /**
- * Example local unit android.app.java.com.duovoc.test, which will execute on the development machine (host).
+ * Example local unit android.app.ks.thinkit.duovoc.test, which will execute on the development machine (host).
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
